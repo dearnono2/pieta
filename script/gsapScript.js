118abecc87acfe137ed4d6a9d231245bedb3e026
dearnono2 <dearnono2@gmail.com>
2023-04-28 12:02:54 +0900

    Feat: 추가

--- script/gsapScript.js
+++ script/gsapScript.js
@@ -1,55 +1,38 @@
-const tl = gsap.timeline();
-tl.from(
-  "#share .moving-txt",
-  { duration: 1, opacity: 0, x: 0, y: 50 },
-  { duration: 1, opacity: 0, x: 0, y: 50 }
-); // 이전 트윈 종료 후 1초 뒤에 시작
-tl.from(
-  "#share .txt-container p",
-  { duration: 1, opacity: 0, y: -50 },
-  "-=0.3"
-); // 이전 트윈 종료 1초 전에 시작
-tl.from(
-  "#share .txt-container a",
-  { duration: 1, opacity: 0, y: -50 },
-  "-=0.5"
-); // 이전 트윈 시작될 때 동시에 실행
-tl.from(".art00", { duration: 0.5, opacity: 0, y: 100 }, 1.5);
-tl.from(".art01", { duration: 0.5, opacity: 0, y: 100 }, 1.5);
-tl.from(".art02", { duration: 0.5, opacity: 0, y: 100 }, 1.5);
-tl.from(".art03", { duration: 0.5, opacity: 0, y: 100 }, 1.5);
-tl.from(".art04", { duration: 1, opacity: 0, y: 100 }, 1.5);
-tl.from(".art05", { duration: 1, opacity: 0, y: 100 }, 1.5);
-tl.from(".art06", { duration: 1, opacity: 0, y: 100 }, 1.5);
-tl.from(".art07", { duration: 0.5, opacity: 0, y: 100 }, 1.5);
-tl.from(".art08", { duration: 0.5, opacity: 0, y: 100 }, 1.5);
-tl.from(".art09", { duration: 0.5, opacity: 0, y: 100 }, 1.5);
-tl.from(".art10", { duration: 0.5, opacity: 0, y: 100 }, 1.5);
-tl.from(".mobile", { duration: 1, opacity: 0, y: 150 }, 1.5);
+/**
+  애니메이션을 순차적으로 주려면 timeline을 사용하자!
+  스크롤 트리거를 사용하려면 타임라인 함수 안에 작성하면 된다.
+ */
 
-// tl.to(object, { opacity: 0.5 }, "<1"); // 이전 트윈 시작 된 후 1초 뒤에 실행
-// tl.to(object2, { x: 200 }, 1); // 타임라인 1초에 실행
+// &
+// section 1 share
+const share = gsap.timeline();
+share
+  // 텍스트 컨테이너 요소들
+  .from("#share .moving-txt", { duration: 1, opacity: 0, x: 0, y: 50 })
+  .from("#share .txt-container p", { duration: 1, opacity: 0, y: -50 }, "-=0.3") // 이전 트윈 종료 0.3초 전에 시작
+  .from("#share .txt-container a", { duration: 1, opacity: 0, y: -50 }, "-=0.5")
+  // 이미지 컨테이너 요소들
+  .from(".art00", { duration: 0.5, opacity: 0, y: 100 }, 1.5)
+  .from(".art01", { duration: 0.5, opacity: 0, y: 100 }, 1.5)
+  .from(".art02", { duration: 0.5, opacity: 0, y: 100 }, 1.5)
+  .from(".art03", { duration: 0.5, opacity: 0, y: 100 }, 1.5)
+  .from(".art04", { duration: 1, opacity: 0, y: 100 }, 1.5)
+  .from(".art05", { duration: 1, opacity: 0, y: 100 }, 1.5)
+  .from(".art06", { duration: 1, opacity: 0, y: 100 }, 1.5)
+  .from(".art07", { duration: 0.5, opacity: 0, y: 100 }, 1.5)
+  .from(".art08", { duration: 0.5, opacity: 0, y: 100 }, 1.5)
+  .from(".art09", { duration: 0.5, opacity: 0, y: 100 }, 1.5)
+  .from(".art10", { duration: 0.5, opacity: 0, y: 100 }, 1.5)
+  .from(".mobile", { duration: 1, opacity: 0, y: 150 }, 1.5);
 
-// const func = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: ".img02",
-//     pin: true,
-//   },
-// });
-
-// func.from(".img00", { duration: 1, opacity: 0, y: 100, stagger: 0.2 });
-// func.from(".img01", { duration: 1, opacity: 0, y: 100 });
-// func.from(".img02", { duration: 1, opacity: 0, x: 100, y: 100 });
-// func.from(".img03", { duration: 1, opacity: 0, x: 100, y: 100 });
-// func.from(".img04", { duration: 1, opacity: 0, x: 100, y: 100 });
-
-let func = gsap.timeline({
-  // yes, we can add it to an entire timeline!
+// &&
+// section2 func
+const func = gsap.timeline({
   scrollTrigger: {
     trigger: ".img02",
     pin: true, // pin the trigger element while active
     start: "top bottom", // when the top of the trigger hits the top of the viewport
-    end: "+=500", // end after scrolling 500px beyond the start
+    end: "+=800", // end after scrolling 500px beyond the start
     scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
     snap: {
       snapTo: "labels", // snap to the closest label in the timeline
@@ -59,28 +42,67 @@ let func = gsap.timeline({
     },
   },
 });
-
-// add animations and labels to the timeline
 func
   .addLabel("start")
   .from(".img00", { scale: 0.3, rotation: 45, autoAlpha: 0 })
-  .addLabel("color")
   .from(".img01", { scale: 0.3, rotation: 45, autoAlpha: 0 })
   .addLabel("spin")
   .to(".img02", { rotation: 360 })
   .addLabel("end")
   .to(".img03", { rotation: 360 })
+  .addLabel("end")
+  .to(".img04", { x: 100, y: -200, rotation: 90 })
   .addLabel("end");
 
-// let srp = gsap.timeline({
-//   // yes, we can add it to an entire timeline!
-//   scrollTrigger: {
-//     trigger: "#srp .txt-container",
-//     pin: true, // pin the trigger element while active
-//     start: "top bottom", // when the top of the trigger hits the top of the viewport
-//   },
-// });
+// &&&
+// section 3 srp
+const srp = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#srp .txt-container",
+    toggleActions: "restart none reverse none",
+    start: "top bottom", // when the top of the trigger hits the top of the viewport
+    end: "+=500",
+  },
+});
+srp
+  .from("#srp .txt-container h2", { duration: 1, opacity: 0, y: -50 })
+  .from("#srp .txt-container p", { duration: 1, opacity: 0, y: -50 }, "-=0.3")
+  .from("#srp .detailViewBtn", { duration: 1, opacity: 0, y: -50 }, "-=0.5");
 
-// srp.from("#srp .txt-container p", { duration: 1, opacity: 0, y: -50 }, "-=0.3"); // 이전 트윈 종료 1초 전에 시작
-// srp.from("#srp .txt-container a", { duration: 1, opacity: 0, y: -50 }, "-=0.5"); // 이전 트윈 시작될 때 동시에 실행
-// srp.from("#srp .art01", { duration: 1, opacity: 0, y: 100, stagger: 0.2 });
+// &&&&
+// section 4 down-app
+const downApp = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#down-app .bg",
+    toggleActions: "restart",
+    start: "top bottom",
+    // end: "+=500",
+  },
+});
+downApp
+  .from("#down-app .txt-container h2", { duration: 1, opacity: 0, x: -100 })
+  .from(
+    "#down-app .txt-container p",
+    { duration: 1, opacity: 0, x: -100 },
+    "-=0.5"
+  )
+  .from(
+    "#down-app .txt-container .store-box",
+    {
+      duration: 1,
+      opacity: 0,
+      x: -100,
+    },
+    "<"
+  );
+
+gsap.to("#down-app .img", {
+  scrollTrigger: {
+    trigger: "#down-app .bg",
+    scrub: 3,
+  },
+  duration: 0.8,
+  x: 100,
+  y: -200,
+  opacity: 1,
+});
